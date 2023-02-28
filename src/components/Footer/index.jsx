18f35386c96231a285d1d24a4fc0342cb743a024
AkifHaciyev <akif6563@mail.ru>
2023-02-28 23:16:52 +0400

    add footer and responsive from footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from './index.module.css';
+import homeButton from '../../assets/icons/home-button.png';
+import compass from '../../assets/icons/compass.png';
+import saveBlack from '../../assets/icons/saveBlack.png';
 
 const Footer = () => {
-	return <footer className={styled.footer}>index</footer>;
+	let activeStyle = {
+		color: '#2f80ed',
+		borderBottom: '3px solid #2f80ed',
+		paddingBottom: '21px',
+	};
+	return (
+		<footer className={styled.footer}>
+			<nav className={styled.nav}>
+				<ul className={styled.ul}>
+					<li className={styled.li}>
+						<NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} className={styled.link} to="/">
+							<img className={styled.img} src={homeButton} alt="" />
+						</NavLink>
+					</li>
+					<li className={styled.li}>
+						<NavLink
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+							className={styled.link}
+							to="/explore"
+						>
+							<img className={styled.img} src={compass} alt="" />
+						</NavLink>
+					</li>
+					<li className={styled.li}>
+						<NavLink
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+							className={styled.link}
+							to="/bookmarks"
+						>
+							<img className={styled.img} src={saveBlack} alt="" />
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</footer>
+	);
 };
 
 export default Footer;
